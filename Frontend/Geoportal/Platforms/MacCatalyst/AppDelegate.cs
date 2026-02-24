7b8c64cc34a743c263374d9277d08a232caf1e4b
Роман Зопиров <irovbyte@outlook.com>
2026-02-24 14:09:08 -0800

    Переход на .NET MAUI

---
+++ Frontend/Geoportal/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Geoportal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
